951c1a4967076ac1acb3794db5147f2008f8c662
Buongws <86525056+Buongws@users.noreply.github.com>
2022-05-15 00:51:16 +0700

    Finished workshop screen (#10)
    * Workshop screen
    
    * fix router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import View from './components/Header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Activities from "./components/Activities";
+import WsPython from "./components/WsPython";
+
+import View from "./components/Header";
 
 function App() {
   return (
-    <View/>
-    //dasda
-
+    <>
+      <View />
+      <Routes>
+        <Route path="/workshop" element={<Activities />} />
+        <Route path="/workshop/:id" element={<WsPython />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/assets/Photo/Index.js
@@ -0,0 +1,25 @@
+import ws1 from "./ws1.png";
+import ws2 from "./ws2.jpg";
+import ws3 from "./ws3.png";
+import ws4 from "./ws4.jpg";
+import ws5 from "./w5.jpg";
+import ws6 from "./w6.png";
+// ------------------------------------------
+import ws4_1 from "./ws4.1.jpg";
+import ws4_2 from "./ws4.2.jpg";
+import ws4_3 from "./ws4.3.jpg";
+import ws4_4 from "./ws4.4.jpg";
+
+const images = {
+  ws1,
+  ws2,
+  ws3,
+  ws4,
+  ws5,
+  ws6,
+  ws4_1,
+  ws4_2,
+  ws4_3,
+  ws4_4,
+};
+export default images;
