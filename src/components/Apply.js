9b651e9a7f9b2e87057df0f8cfc13da1261f9352
Chau83 <94224659+Chau83@users.noreply.github.com>
2022-05-30 01:03:59 +0700

    done

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -3,13 +3,13 @@ import React from "react";
 function Apply() {
   return (
     <div>
-      <div className="h-80 bg-gradient-to-br from-gray-800 to-blue-300  text-white items-center justify-center flex flex-col">
+      <div className="h-[250px] bg-gradient-to-br from-gray-800 to-blue-300  text-white items-center justify-center flex flex-col lg:h-80">
         <h3 className="text-slate-300 text-center ">ĐĂNG KÝ THÀNH VIÊN</h3>
-        <h2 className="text-5xl font-extrabold text-center pb-3 ">
+        <h2 className="text-[28px] font-extrabold text-center pb-3 lg:text-5xl">
           Ứng Tuyển Ngay
         </h2>
         <div className="pt-3.5 h-12 w-32">
-          <button className="items-center bg-blue-600 h-12 w-32 rounded-lg ease-out duration-300 hover:h-10 hover:w-28 shadow-md">
+          <button className="items-center bg-blue-600 h-12 w-32 rounded-lg ease-out duration-300 shadow-md lg:hover:h-10 lg:hover:w-28 ">
             Aplly Now
           </button>
         </div>
--- src/components/IntroAbout.js
+++ src/components/IntroAbout.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function IntroAbout() {
   return (
-    <div className="h-[480px] bg-bgki1 from-sky-800 bg-cyan-600  px-36 text-white items-center flex">
+    <div className=" bg-bgki1 from-sky-800 bg-cyan-600 text-white justify-center items-center flex h-[200px]  lg:justify-start lg:pl-[200px] lg:h-[480px]">
       <div>
-        <h6 className="font-semibold text-[20px] mb-6 text-slate-300">
+        <h6 className="font-semibold text-center text-[18px] mb-6 text-slate-300 lg:text-[20px] lg:text-left">
           GIỚI THIỆU
         </h6>
-        <h1 className="font-bold text-[100px] mb-14 leading-none ">
+        <h1 className=" font-semibold mb-14 leading-none text-[40px] lg:text-[100px] lg:font-bold">
           Về chúng tôi
         </h1>
       </div>
