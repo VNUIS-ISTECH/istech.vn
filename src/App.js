d43f687370cdc9c7309304bc5f53a02384f95751
HoangDat0412 <hoang2811dat@gmail.com>
2022-12-12 20:52:00 +0700

    chị thư dở hơi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import logo from './logo.svg';
 import './App.css';
-import View from './components/Header';
+import Header from './components/Header';
 
 function App() {
-  print('hello ww')
   return (
-    <View/>
-    //dasda
+   <div>
+     <Header/>
+     <h2>
+      Đm thằng tưởng
+     </h2>
+     <h1>
+      spdok
+     </h1>
 
+     <ul>
+      <li>
+        thu chơi đơ s
+      </li>
+      <li>
+        tưởng ăn cám 
+      </li>
+      <li>
+        sad boiii 
+      </li>
+     </ul>
+   </div>
   );
 }
 
 export default App;
+//tk da
