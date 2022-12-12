122b3cacbc07dd9b23b42da2ad67d9a4e5209b0a
= <=>
2022-12-12 20:50:21 +0700

    test reject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import './App.css';
-import View from './components/Header';
-
-function App() {
-  print('hello ww')
-  return (
-    <View/>
-    //dasda
-
-  );
-}
-
-export default App;
+requestAnimationFrame
